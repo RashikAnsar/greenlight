067cc26112fbdf5378011c22aa4b6fe20ff4a629
RashikAnsar <sk.ansar46@gmail.com>
2021-07-08 10:11:37 +0530

    JSON API setup

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: Available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	// js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
