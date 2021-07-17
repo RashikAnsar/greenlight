93064909d5a258cb48e357e015c79e734ea0bd75
RashikAnsar <sk.ansar46@gmail.com>
2021-07-17 11:55:43 +0530

    graceful shutdown of background processes

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -130,7 +130,9 @@ func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http
 }
 
 func (app *application) background(fn func()) {
+	app.wg.Add(1)
 	go func() {
+		defer app.wg.Done()
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.PrintError(fmt.Errorf("%s", err), nil)
--- cmd/api/server.go
+++ cmd/api/server.go
@@ -34,7 +34,17 @@ func (app *application) server() error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		shutdownError <- srv.Shutdown(ctx)
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+
+		app.logger.PrintInfo("completing background tasks", map[string]string{
+			"addr": srv.Addr,
+		})
+
+		app.wg.Wait()
+		shutdownError <- nil
 	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
