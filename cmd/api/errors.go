0c27553ff5d6cc52206054be728199538adc1102
RashikAnsar <sk.ansar46@gmail.com>
2021-07-17 23:02:44 +0530

    Generating authentication tokens

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -50,3 +50,8 @@ func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Requ
 	message := "unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
+
+func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
+	message := "invalid authentication credentials"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
